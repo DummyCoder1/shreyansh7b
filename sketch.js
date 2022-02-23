a95c773523622ef4e661ee93995b34d65c94b8be
DummyCoder1 <100186733+DummyCoder1@users.noreply.github.com>
2022-02-23 13:13:51 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var box;
+function setup() {
+  createCanvas(400, 400);
+  
+  //1. Set width, height, x and y position in "createSprite()"
+  box = createSprite(40,40,40,40 );
+  
+  //2. Set sprite color to "white"
+  box.shapeColor =("white") ; 
+  
+  //3. Set velocity of sprite for vertical movement
+  box.velocityY=-0.5;
+}
+function draw() {
+  background("orange");
+  drawSprites();
+} 
